0029dcde0944a55f2159ca103203b78af5ed91c1
nicolas-sacco <nicolas.sacco@mail.utoronto.ca>
2024-09-14 23:20:51 -0400

    final-v5

--- src/header-components/aboutme.tsx
+++ src/header-components/aboutme.tsx
@@ -4,7 +4,14 @@ function About(){
             <span className="about-text">Nicolas Sacco</span>
             <div className="tooltip">
                 <div className="tooltip-content">
-                    <p>Hi! I'm Nicolas Sacco, a passionate developer with a knack for creating efficient and scalable applications. I love working with modern technologies and solving complex problems.</p>
+                    <p> Hi! I'm Nicolas Sacco, an Industrial Engineering major and Business minor at the University of Toronto. 
+                        I love working with modern technologies to solve complex problems within financial markets!<br /><br />
+                        
+                        This Full-Stack Black-Scholes Option Pricing Model is a tool that traders across the globe 
+                        can use to price the theoretical value of options in real-time! <br /><br />
+                        
+                        This model allows traders to 
+                        capitalize on mispricings in option markets on any stock</p>
                 </div>
             </div>
         </div>
